79ae4769d6ea6fe34ce8edde7b375de438772063
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-06-07 13:38:18 +0100

    [LinkedList] improved code

--- Program.cs
+++ Program.cs
@@ -10,16 +10,8 @@ namespace Practise
         {
             System.Console.WriteLine("Sparse Array -> ");
             SparseArrays.execute();
-            System.Console.WriteLine("\n\nPrint Linked List-> ");
-            PrintLinkedLists.execute();
-            System.Console.WriteLine("\n\nInsert Tail Linked List-> ");
-            InsertAtTail.execute();
-            System.Console.WriteLine("\n\nInsert Head Linked List-> ");
-            InsertAtHead.execute();
-            System.Console.WriteLine("\n\nDelete Linked List-> ");
-            DeleteNode.execute();
-            System.Console.WriteLine("\n\nPrint in Reverse Linked List-> ");
-            PrintInReverse.execute();
+            System.Console.WriteLine("\n\nLinked List Operations-> ");
+            LinkedList.execute();
         }
     }
 }
---
+++ Programs/LinkedLists/LinkedList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practise.Programs.LinkedLists
+{
+    public class LinkedList
+    {
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+            }
+
+            public void insertNodeAtHead(int data)
+            {
+                SinglyLinkedListNode newHead = new SinglyLinkedListNode(data);
+                newHead.next = head;
+                head = newHead;
+            }
+
+            public void insertNodeAtTail(int data)
+            {
+                if (head == null)
+                    head = new SinglyLinkedListNode(data);
+                else
+                {
+                    SinglyLinkedListNode temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = new SinglyLinkedListNode(data);
+                }
+            }
+
+            public void deleteNodeAtPosition(int position)
+            {
+                SinglyLinkedListNode temp = head;
+
+                if (head == null)
+                    return;
+
+                if (position == 0)
+                {
+                    head = head.next;
+                }
+
+                while (temp.next != null)
+                {
+                    if ((position-- - 1) == 0)
+                    {
+                        temp.next = temp.next.next;
+                    }
+                    temp = temp.next;
+                }
+            }
+
+            public void reversePrint()
+            {
+                reversePrintHelper(head);
+            }
+
+            private void reversePrintHelper(SinglyLinkedListNode root)
+            {
+                if (root == null)
+                    return;
+
+                reversePrintHelper(root.next);
+                System.Console.WriteLine(root.data);
+            }
+
+            public void print()
+            {
+                SinglyLinkedListNode temp = head;
+                while (temp != null)
+                {
+                    System.Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+
+        public static void execute()
+        { 
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            llist.insertNodeAtHead(13);
+            llist.insertNodeAtHead(27);
+            llist.insertNodeAtHead(10);
+            llist.insertNodeAtHead(0);
+            llist.insertNodeAtTail(16);
+            llist.insertNodeAtTail(90);
+
+            llist.deleteNodeAtPosition(2);
+
+            llist.reversePrint();
+            llist.print();
+        }
+    }
+}
